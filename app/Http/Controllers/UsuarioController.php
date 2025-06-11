93f27d2b72c956afa8ad499772f5158233458213
KeidyC <kcoellogaray@birt.eus>
2025-06-11 19:23:39 +0200

    primer Commit

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+
+class UsuarioController extends Controller
+{
+    public function procesarNombre(Request $request){
+        $data = $request->all();
+        $nombre = $data["nombre"];
+        $apellido = $data["apellido"];
+        $dni = $data["apellido"];
+ 
+        $usuario = new Usuario($nombre,$apellido,$dni);
+
+        $nombreCompleto = $usuario->obtenerNombreCompleto();
+
+        return response()->json([
+            "status" => "Success",
+            "nombreCompleto" => $nombreCompleto,
+            "code" => 200
+            
+        ],200);
+    }
+}
---
+++ app/Models/Usuario.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    use HasFactory;
+    protected $nombre;
+    protected $apellido;
+    protected $dni;
+
+    public function __construct($nombre,$apellido,$dni){
+        $this->nombre=$nombre;
+        $this->apellido=$apellido;
+        $this->dni= $dni;
+    }
+    
+    public function obtenerNombreCompleto(){
+        return "Me llamo " . $this->nombre . ' ' . $this->apellido . ' y mi dni es ' . $this->dni;
+    }
+}
+
+
+
